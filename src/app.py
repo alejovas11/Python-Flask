fbe8811dbc494e63e888d3c93e82bf812958665c
alejovas11 <70924940+alejovas11@users.noreply.github.com>
2020-11-30 19:12:08 -0500

    app

--- src/app.py
+++ src/app.py
@@ -4,16 +4,11 @@ from bson import json_util
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
-app.config['MONGO_URI']='mongodb://localhost:27017/pythonmongodb'
-
-#Le pasamos la configuracion a PyMongo
+app.config['MONGO_URI']='mongodb://localhost:27017/citas'
 mongo = PyMongo(app)
 
-#POST
-@app.route('/customer', methods=['POST'])
-#Funcion para crear el usuario
+@app.route('/addCita', methods=['POST'])
 def create_user():
-    #Recibiendo datos
     name = request.json['name']
     lastName = request.json['lastName']
     identification = request.json['identification']
@@ -22,9 +17,7 @@ def create_user():
     neighborhood = request.json['neighborhood']
     phone = request.json['phone']
 
-    #Validacion sencilla de que estamos recibiendo los datos
     if name and lastName and identification and birthDate and city and neighborhood and phone:
-        #Esto retorna un id
         id = mongo.db.customers.insert(
             {
                 'name' : name,
@@ -36,7 +29,6 @@ def create_user():
                 'phone' : phone
             }
         )
-        #Descripcion del nuevo dato que se ha creado
         response = {
             'id': str(id),
             'name' : name,
@@ -53,27 +45,19 @@ def create_user():
     
     return {'message': 'received'}
 
-#GET ALL USERS
-@app.route('/customers', methods=['GET'])
+@app.route('/getCitas', methods=['GET'])
 def get_users():
     customers = mongo.db.customers.find()
-    response = {"quotesDB": customers}
+    response = {"citas": customers}
     return Response(json_util.dumps(response),  mimetype='application/json')
-    # response = json_util.dumps(customers)
-    # #Response es para que se vea como un json en vez de un String
-    # return Response(response, mimetype='application/json')
 
-#GET USER BY ID
 @app.route('/customer/<id>', methods=['GET'])
 def get_user(id):
-    #Convertimos a un object id el id que nos manda el usuario
     user = mongo.db.customers.find_one({'_id': ObjectId(id)})
-    #Lo convertimos a un Json
     response = json_util.dumps(user)
     return Response(response, mimetype='application/json')
 
-#DELETE
-@app.route('/customer/<id>', methods=['DELETE'])
+@app.route('/deleteCita/<id>', methods=['DELETE'])
 def delete_user(id):
     mongo.db.customers.delete_one({'_id': ObjectId(id)})
     response = jsonify({
@@ -81,8 +65,7 @@ def delete_user(id):
     })
     return response
 
-#PUT
-@app.route('/customer/<id>', methods=['PUT'])
+@app.route('/updateCita/<id>', methods=['PUT'])
 def update_user(id):
     name = request.json['name']
     lastName = request.json['lastName']
@@ -107,16 +90,12 @@ def update_user(id):
     })
     return response
 
-
-#Manejador de errores
 @app.errorhandler(404)
 def not_found(error=None):
-    #jsonify permite añadir mas propiedades
     response = jsonify({
         'message': 'Resource not found: ' + request.url,
         'status': 404
     })
-    #Cambiamos el codigo de estatus
     response.status_code = 404
     return response
 
